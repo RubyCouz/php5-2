39810069c963184f908c0a851a57f7beb69605f1
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-26 18:07:02 +0200

    exo2

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 1</title>
+  </head>
+  <body>
+    <p>
+<?php
+$month = array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre');
+echo $month[2];
+?>
+    </p>
+  </body>
+</html>
